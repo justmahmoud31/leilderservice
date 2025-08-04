d60f032ca19693358ba5ab77c79c981deddd7c20
justmahmoud31 <justmahmoud3152003@gmail.com>
2025-08-04 14:55:23 +0300

    Fixed grid

--- app/verhuizen/page.tsx
+++ app/verhuizen/page.tsx
@@ -118,7 +118,7 @@ function page() {
           </h3>
         </div>
       </FadeIn>
-      <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 p-4">
+      <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3  p-4">
         {packs.map((pack, index) => (
           <FadeIn direction="up" key={index}>
             <Card
